2f8eb160e6823e50b841df1fc5c6d703f915c3bb
JakeNull <jake9828@gmail.com>
2018-04-12 18:51:01 -0400

    Begin

--- script/main.js
+++ script/main.js
@@ -6,8 +6,4 @@ $(document).ready(function() {
     awake(); // Always awake first
     switchScene(new MenuScene()); // Setup first scene of the game
     init(); // Init game
-
-    // Does it work?
-    // Please ignore xml shit, I need to do this BS omg I hate collage, I hate life
-    // whatever
 });
