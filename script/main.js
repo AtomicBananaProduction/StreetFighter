8aa056b00a7fe548ecd31eef10b68a54d0221487
JakeNull <jake9828@gmail.com>
2018-04-12 18:53:05 -0400

    Delete .idea

--- script/main.js
+++ script/main.js
@@ -6,4 +6,6 @@ $(document).ready(function() {
     awake(); // Always awake first
     switchScene(new MenuScene()); // Setup first scene of the game
     init(); // Init game
+
+    //Keep test
 });
