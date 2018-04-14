b48515a8269af19faed62250cc9ed7dab914ffc7
StrikerX55 <38334337+StrikerX55@users.noreply.github.com>
2018-04-13 22:22:49 -0400

    Let Background outside class

--- script/scenes/game_scene.js
+++ script/scenes/game_scene.js
@@ -1,13 +1,13 @@
+let background;//Background if u couldnt tell
+
 class GameScene extends Scene {
-	
-	let background;//Background if u couldnt tell
 
     begin() {
         super.begin();
 		
 		background = new GameObject(
         new Vector2D(0, 0), // Object's position
-        "assets/streetBackground.png", // Image's path
+        "assets/StreetBackground.png", // Image's path
         0.0, // Mass of object IGNORE UNTIL WE GET TO PLAYER
         0.0, // Gravity IGNORE UNTIL WE GET TO PLAYER
         false); // If enable gravity SET TO FALSE FOR NOW
